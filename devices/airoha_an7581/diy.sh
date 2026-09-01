5e95c8a2d30f82b2a8d2012604b600e620988954
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2026-09-01 20:25:34 +0800

    update

--- devices/airoha_an7581/diy.sh
+++ devices/airoha_an7581/diy.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+shopt -s extglob
+
 curl -sL https://patch-diff.githubusercontent.com/raw/openwrt/openwrt/pull/24883.patch | git apply -v
+
+rm -rf package/boot
+
+git_clone_path main https://github.com/openwrt/openwrt package/boot
+
+wget -N https://github.com/openwrt/openwrt/raw/refs/heads/main/target/linux/airoha/dts/an7581-nokia_xg-040g-md-common.dtsi -P target/linux/airoha/dts/
+wget -N https://github.com/openwrt/openwrt/raw/refs/heads/main/target/linux/airoha/dts/an7581-nokia_xg-040g-md-ubi.dts -P target/linux/airoha/dts/
+wget -N https://github.com/openwrt/openwrt/raw/refs/heads/main/target/linux/airoha/dts/an7581-nokia_xg-040g-md.dts -P target/linux/airoha/dts/
+wget -N https://github.com/openwrt/openwrt/raw/refs/heads/main/target/linux/airoha/dts/an758x-nokia_xg-040g-common.dtsi -P target/linux/airoha/dts/
+wget -N https://github.com/openwrt/openwrt/raw/refs/heads/main/target/linux/airoha/dts/an758x-nokia_xg-040g-stock-parts.dtsi -P target/linux/airoha/dts/
+wget -N https://github.com/openwrt/openwrt/raw/refs/heads/main/target/linux/airoha/dts/an758x-nokia_xg-040g-ubi-parts.dtsi -P target/linux/airoha/dts/
